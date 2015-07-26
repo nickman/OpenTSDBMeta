23c58133c9eea9a6d818ba652441e7a20bd2a10d
nickman <whitehead.nicholas@gmail.com>
2015-07-26 07:47:35 -0400

    DevSynch

---
+++ src/main/java/com/heliosapm/phoenix/udf/BinaryToInteger.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.heliosapm.phoenix.udf;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import jline.internal.Log;
+
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.phoenix.coprocessor.GroupedAggregateRegionObserver;
+import org.apache.phoenix.expression.Expression;
+import org.apache.phoenix.expression.function.ScalarFunction;
+import org.apache.phoenix.schema.tuple.Tuple;
+import org.apache.phoenix.schema.types.PDataType;
+import org.apache.phoenix.schema.types.PInteger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>Title: BinaryToInteger</p>
+ * <p>Description: Phoenix UDF to convert OpenTSDB binary values in HBase tables to integers</p> 
+ * <p>Company: Helios Development Group LLC</p>
+ * @author Whitehead (nwhitehead AT heliosdev DOT org)
+ * <p><code>com.heliosapm.phoenix.udf.BinaryToInteger</code></p>
+ */
+
+public class BinaryToInteger extends ScalarFunction {
+	/** The SQL name of this function */
+	public static final String NAME = "BINTOINT";
+	
+    private static final Logger log = LoggerFactory .getLogger(BinaryToInteger.class);
+
+	/**
+	 * Creates a new BinaryToNumber
+	 */
+	public BinaryToInteger() {	
+		log.warn("Created BinaryToInteger instance");
+	}
+
+	/**
+	 * Creates a new BinaryToNumber
+	 * @param children not sure
+	 */
+	public BinaryToInteger(final List<Expression> children) {
+		super(children);
+		log.warn("Created BinaryToInteger instance");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.apache.phoenix.expression.Expression#evaluate(org.apache.phoenix.schema.tuple.Tuple, org.apache.hadoop.hbase.io.ImmutableBytesWritable)
+	 */
+	@Override
+	public boolean evaluate(final Tuple tuple, final ImmutableBytesWritable ptr) {
+		log.warn("Calling BinaryToInteger: {}", tuple);
+		try {
+	        Expression arg = getChildren().get(0);
+	        if (!arg.evaluate(tuple, ptr)) {
+	            return false;
+	        }
+	
+	        int targetOffset = ptr.getLength();
+	        if (targetOffset == 0) {
+	            return true;
+	        }
+	        final int x = ByteBuffer
+	        		.wrap(ptr.get())
+	        		.asIntBuffer()
+	        		.get(0);
+	        log.warn("Int generated: {}", x);
+	        byte[] byteValue = getDataType().toBytes(x);
+	        ptr.set(byteValue);
+	        log.warn("Done");
+	        return true;
+	        
+//	        byte[] source = ptr.get();
+//	        final int len = Math.min(source.length, 4);
+//	        byte[] target = new byte[4];
+//	        System.arraycopy(source, 0, target, 0, len);
+//	        final int x = ByteBuffer.wrap(target).asIntBuffer().get(0);
+//	        byte[] byteValue = getDataType().toBytes(x);
+//	        ptr.set(byteValue);
+//	        return true;
+		} catch (Throwable t) {
+			log.error("Failed to execute bintoint", t);
+			return false;
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.apache.phoenix.schema.PDatum#getDataType()
+	 */
+	@Override
+	public PDataType getDataType() {
+		return PInteger.INSTANCE;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.apache.phoenix.expression.function.FunctionExpression#getName()
+	 */
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+}
---
+++ src/test/java/com/heliosapm/phoenix/udf/BinaryToIntegerTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.heliosapm.phoenix.udf;
+
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.phoenix.expression.Expression;
+import org.apache.phoenix.expression.LiteralExpression;
+import org.apache.phoenix.expression.function.InstrFunction;
+import org.apache.phoenix.schema.SortOrder;
+import org.apache.phoenix.schema.types.PDataType;
+import org.junit.Test;
+
+/**
+ * <p>Title: BinaryToIntegerTest</p>
+ * <p>Description: </p> 
+ * <p>Company: Helios Development Group LLC</p>
+ * @author Whitehead (nwhitehead AT heliosdev DOT org)
+ * <p><code>com.heliosapm.phoenix.udf.BinaryToIntegerTest</code></p>
+ */
+
+public class BinaryToIntegerTest {
+	
+    public static void inputExpression(String value, PDataType dataType, String strToSearch,Integer expected, SortOrder order) throws SQLException{
+        Expression inputArg = LiteralExpression.newConstant(value,dataType,order);
+        
+        Expression strToSearchExp = LiteralExpression.newConstant(strToSearch,dataType);
+        List<Expression> expressions = Arrays.<Expression>asList(inputArg,strToSearchExp);
+        Expression instrFunction = new InstrFunction(expressions);
+        ImmutableBytesWritable ptr = new ImmutableBytesWritable();
+        instrFunction.evaluate(null,ptr);
+        Integer result = (Integer) instrFunction.getDataType().toObject(ptr);
+        assertTrue(result.compareTo(expected) == 0);
+        
+    }
+    	
+    @Test
+    public void testBasicConversion() {
+    	
+    }
+    
+    public static void main(String[] args) {
+    	log("BinaryToIntegerTest");
+    	Connection conn = null;
+    	PreparedStatement ps = null;
+    	ResultSet rset = null;
+    	try {
+    		Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
+    		Properties p = new Properties();
+    		System.setProperty("phoenix.functions.allowUserDefinedFunctions", "true");
+    		p.setProperty("phoenix.functions.allowUserDefinedFunctions", "true");
+    		conn = DriverManager.getConnection("jdbc:phoenix:localhost", p);
+    		ps = conn.prepareStatement("select PK, BINTOINT(\"tagv\") from \"tsdb-uid\" where \"tagv\" is not null and PK = ?");
+    		ps.setString(1, "/proc");
+    		rset = ps.executeQuery();
+    		ResultSetMetaData rsmd = rset.getMetaData();
+    		int cnt = 1;
+    		for(int i = 1; i < rsmd.getColumnCount() + 1; i++) {
+    			log(String.format("Col #%s: Name: [%s], Type: [%s]", i, rsmd.getColumnName(i), rsmd.getColumnTypeName(i)));
+    		}
+    		while(rset.next()) {
+    			log("tagv: [" + rset.getObject(2) + "]");
+    		}
+    	} catch (Exception ex) {
+    		ex.printStackTrace(System.err);
+    	} finally {
+    		if(rset!=null) try { rset.close(); } catch (Exception x) {/* No Op */}
+    		if(ps!=null) try { ps.close(); } catch (Exception x) {/* No Op */}
+    		if(conn!=null) try { conn.close(); } catch (Exception x) {/* No Op */}
+    	}
+    }
+    
+    public static void log(Object msg) {
+    	System.out.println(msg);
+    }
+
+}
